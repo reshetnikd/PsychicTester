c4fbefc659911e40f408bd7e99d853840fadccf5
reshetnikd <dmitry.reshetnik@icloud.com>
2020-07-23 16:10:15 +0300

    Initial Commit

---
+++ PsychicTester WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  PsychicTester WatchKit Extension
+//
+//  Created by Dmitry Reshetnik on 23.07.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
